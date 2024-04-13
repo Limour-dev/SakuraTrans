92276d9aec4576280a4a6cd02ceebdced1791349
Limour <93720049+Limour-dev@users.noreply.github.com>
2024-04-14 00:09:39 +0800

    Add files via upload

--- transJa.py
+++ transJa.py
@@ -1,5 +1,26 @@
 import requests
 import traceback
+import re
+
+reg1 = re.compile(r'(.)\1{3,}')
+
+
+def reg1_replace(match):
+    return match.group(1) * 3
+
+
+reg2 = re.compile(r'(.{2,}?)\1{2,}')
+
+
+def reg2_replace(match):
+    return match.group(1)
+
+
+def removeRep(_s:str):
+    retn = reg1.sub(reg1_replace, _s)
+    retn = reg2.sub(reg2_replace, retn)
+    return retn
+
 
 header = {
     'Content-Type': 'application/json',
@@ -24,7 +45,7 @@ def transJa_One(_s: str):
         ],
         "temperature": 0.1,
         "top_p": 0.3,
-        "max_tokens": 1000,
+        "max_tokens": int(len(_s) * 1.5) + 10,
         "frequency_penalty": 0,
         "do_sample": True,
         "top_k": 40,
@@ -35,14 +56,20 @@ def transJa_One(_s: str):
     r = r.json()
     if 'choices' in r:
         return r['choices'][0]['message']['content']
+    else:
+        print(r)
     return ''
 
 
 def transJa(_s: str):
+    _s = removeRep(_s)
     for i in range(3):
         try:
             retn = transJa_One(_s)
             if retn:
+                retn = removeRep(retn)
+                if retn.startswith('「') and not retn.endswith('」'):
+                    retn += '」'
                 return retn
         except:
             print(traceback.format_exc())
